e1cff4553ac181bc8677c5bbe7ab45a265091add
Alejandro Nathanael Martin del Campo Barba <alejandro@MacBook-Air-de-Alejandro.local>
2023-05-06 15:21:44 -0600

    se agregan bases de datos

---
+++ Semestre2/bases_datos/help.py
@@ -0,0 +1,25 @@
+# function that read all excel files in a folder, and join them in a single dataframe
+import pandas as pd
+import glob
+
+folder_path = "./data/gdl"
+
+
+def read_all_excel_files(folder_path):
+    all_files = glob.glob(folder_path + "*.xlsx")
+    df_list = []
+    for file in all_files:
+        df = pd.read_excel(file)
+        df_list.append(df)
+    return pd.concat(df_list)
+
+joined_df = read_all_excel_files(folder_path)
+print(joined_df)
+
+# function that read all excel files in a folder create a dataframe for each one and print the head of each one, also print the name of each file
+def read_all_excel_files_and_print_head(folder_path):
+    all_files = glob.glob(folder_path + "*.xlsx")
+    for file in all_files:
+        df = pd.read_excel(file)
+        print(df.head())
+        print(file)
